6a0b446adf53ceada961e8c2ca08c6aabe64eaf7
ninatom <ntomasev@usc.edu>
2022-11-09 20:20:58 -0800

    add project-3

---
+++ Project-3/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    
+    
+   
+    
+        $('#plantgif').click(function(){
+        $('#ptxt').toggle();
+     });
+
+    
+    
+  $('#keyboard').click(function () {
+//  $('.infopanel').toggle(2000);     
+        $(this).toggleClass("big");
+    });
+  
+    
+    
+
+    
+});
+
