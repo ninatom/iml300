a656d2960a16c40ff03aafc793e4f1d3373649e4
ninatom <ntomasev@usc.edu>
2022-11-28 16:19:50 -0800

    yes

--- Project-3/script.js
+++ Project-3/script.js
@@ -1,23 +1,18 @@
-
 $(document).ready(function () {
     
-    
-   
-    
+
         $('#plantgif').click(function(){
         $('#ptxt').toggle();
      });
 
-    
-    
+  
   $('#keyboard').click(function () {
 //  $('.infopanel').toggle(2000);     
         $(this).toggleClass("big");
     });
-  
-    
-    
 
-    
+
 });
+
+
 
