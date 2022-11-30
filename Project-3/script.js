10153a2f7341fa56386622b16078fcaca187b2ab
ninatom <ntomasev@usc.edu>
2022-11-30 11:18:09 -0800

    t

--- Project-3/script.js
+++ Project-3/script.js
@@ -1,153 +1,147 @@
 $(document).ready(function () {
-    
-//    $('#plantgif').click(function(){
-//        $('#ptxt').toggle();
-//     });
-//
-//  
 //  $('#keyboard').click(function () {
 //  $('.infopanel').toggle(2000);     
-//        $(this).toggleClass("big");
-//    });
-    
-    
 
-  $( ".hoverButton" ).mouseover(function() { 
-    $(".HoverState").hide();
-    $(this).next().show();
-});
-  $( ".hoverButton" ).mouseout(function() {
-    $(".HoverState").hide();
-});
-    
+    //    $('#plantgif').click(function(){
+    //        $('#ptxt').toggle();
+    //     });
+    //
+    //  
+    //  $('#keyboard').click(function () {
+    //  $('.infopanel').toggle(2000);     
+    //        $(this).toggleClass("big");
+    //    });
 
 
-//    $('#box2').hide();
-//   
-    
-    
+
+    $(".hoverButton").mouseover(function () {
+        $(".HoverState").hide();
+        $(this).next().show();
+    });
+    $(".hoverButton").mouseout(function () {
+        $(".HoverState").hide();
+    });
+
+
+
+    //    $('#box2').hide();
+    //   
+
     $('#box1').hover(function () {
         $('#box1').hide();
     }, function () {
         $('#box1').fadeIn();
     });
-    
+
     $('#box2').hover(function () {
         $('#box2').hide();
     }, function () {
         $('#box2').fadeIn();
     });
-    
-       $('#box3').hover(function () {
+
+    $('#box3').hover(function () {
         $('#box3').hide();
     }, function () {
         $('#box3').fadeIn();
-    }); 
-    
-           $('#box4').hover(function () {
+    });
+
+    $('#box4').hover(function () {
         $('#box4').hide();
     }, function () {
         $('#box4').fadeIn();
-    }); 
-    
-               $('#box5').hover(function () {
+    });
+
+    $('#box5').hover(function () {
         $('#box5').hide();
     }, function () {
         $('#box5').fadeIn();
-    }); 
-    
-               $('#box6').hover(function () {
+    });
+
+    $('#box6').hover(function () {
         $('#box6').hide();
     }, function () {
         $('#box6').fadeIn();
-    }); 
-    
-               $('#box7').hover(function () {
+    });
+
+    $('#box7').hover(function () {
         $('#box7').hide();
     }, function () {
         $('#box7').fadeIn();
-    }); 
-    
-    
-               $('#box8').hover(function () {
+    });
+
+
+    $('#box8').hover(function () {
         $('#box8').hide();
     }, function () {
         $('#box8').fadeIn();
-    }); 
-  
-    
-                   $('#box9').hover(function () {
+    });
+
+
+    $('#box9').hover(function () {
         $('#box9').hide();
     }, function () {
         $('#box9').fadeIn();
-    }); 
-    
-    
-               $('#box10').hover(function () {
+    });
+
+
+    $('#box10').hover(function () {
         $('#box10').hide();
     }, function () {
         $('#box10').fadeIn();
-    }); 
-    
-    
-               $('#box11').hover(function () {
+    });
+
+
+    $('#box11').hover(function () {
         $('#box11').hide();
     }, function () {
         $('#box11').fadeIn();
-    }); 
-        
-    
-                $('#box12').hover(function () {
+    });
+
+
+    $('#box12').hover(function () {
         $('#box12').hide();
     }, function () {
         $('#box12').fadeIn();
-    }); 
-       
-    
-                   $('#box13').hover(function () {
+    });
+
+
+    $('#box13').hover(function () {
         $('#box13').hide();
     }, function () {
         $('#box13').fadeIn();
-    }); 
-        
-                    $('#box14').hover(function () {
+    });
+
+    $('#box14').hover(function () {
         $('#box14').hide();
     }, function () {
         $('#box14').fadeIn();
-    }); 
-       
-                    $('#box15').hover(function () {
+    });
+
+    $('#box15').hover(function () {
         $('#box15').hide();
     }, function () {
         $('#box15').fadeIn();
-    }); 
-       
-                    $('#box16').hover(function () {
+    });
+
+    $('#box16').hover(function () {
         $('#box16').hide();
     }, function () {
         $('#box16').fadeIn();
-    }); 
-       
-    
-    
-    
-//      $('#box1').hover(function(){
-//          $('#box2').hide();
-//      });
+    });
 
 
-    
 
-    
-    
-    
-    
-    
-    
+
+    //      $('#box1').hover(function(){
+    //          $('#box2').hide();
+    //      });
+
+
+
+
+
+
+
+
+
 });
-
-
-
-
-
-
