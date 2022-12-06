b42becc8699243ee174b2c64bdae9ca35e15d15f
ninatom <ntomasev@usc.edu>
2022-12-05 19:14:53 -0800

    choco

---
+++ Project-3/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(231, 229, 228);
+    
+    fill(148, 47, 7);
+    for (let col = 0; col <= width; col += 60) {
+        for (let row = 0; row <= height; row += 60) {
+            push();
+            translate(col, row);
+            rotate(frameCount * 0.02);
+            rectMode(CENTER);
+            noStroke();
+            fill(91, 62, 56);
+            rect(0, 0, 20, 60);
+            pop();
+        }
+    }
+}
